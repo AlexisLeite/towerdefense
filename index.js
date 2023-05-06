cc224cc0de396a96a0873b4e38b0a76a97b13b4c
Alexis Leite <alexisleite.uy@gmail.com>
2023-05-06 16:33:19 -0300

    added visits

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3030;
 
+let visits = 0;
 app.get("*", (req, res) => {
-  res.json({ params: req.params, query: req.query });
+  visits++;
+  res.json({ visits, params: req.params, query: req.query });
 });
 
 app.listen(PORT, () => {
